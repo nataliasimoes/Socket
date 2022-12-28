51f7e69f84be0d53a811015d463e4cc335da14e7
Francisca Natália Simões <francisca.araujo@imobme.com>
2022-12-28 08:20:54 -0300

    Add comments

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
 import socket
 import threading
 
-def sum(connection):
+# Função que realiza a soma dos números recebidos e recebe a conexão como parâmetro
+def sum(connection): 
     i=0
     soma=0
     while i<2:
@@ -13,10 +14,10 @@ def sum(connection):
             break
         print('Número recebido: %s' %data.decode())
         i+=1
-    connection.send(str(soma).encode())
+    connection.send(str(soma).encode()) # Envia a soma para o cliente
 
-class sumThread(threading.Thread):
-    def __init__(self, connection):
+class sumThread(threading.Thread): # Cria uma thread para cada conexão
+    def __init__(self, connection): 
         threading.Thread.__init__(self)
         self.connection = connection
 
@@ -27,19 +28,17 @@ class sumThread(threading.Thread):
         self.connection.close()
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-server.bind (('localhost', 7777))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria o socket
+server.bind (('localhost', 7777)) 
 print('Aguardando conexões.\n')
 server.listen(1)
 
-i=0
-soma=0
 try:
     print("Aguardando mensagem do cliente.\n")
     while True:
-        connection, address = server.accept()
-        newThread = sumThread(connection)
-        newThread.start()
+        connection, address = server.accept() # Aceita a conexão
+        newThread = sumThread(connection) # Cria  uma nova thread para a conexão
+        newThread.start() # Inicia a thread e chamada a função sum
         
 except socket.error as e: 
     print ("Socket error: %s" %str(e)) 
