4157905303b24cfcab435775a4c32fc212a384fa
nataliasimoes <natlia932@gmail.com>
2022-12-27 21:43:40 -0300

    Add thread

--- server/server.py
+++ server/server.py
@@ -1,16 +1,10 @@
 import socket
+import threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-server.bind (('localhost', 7777))
-print('Aguardando conexões.\n')
-server.listen(1)
-
-connection, address = server.accept()
-i=0
-soma=0
-try:
-    print("Aguardando mensagem do cliente.\n")
-    while True:
+def sum(connection):
+    i=0
+    soma=0
+    while i<2:
         require = 'Digite um número: '
         connection.send(require.encode())
         data = connection.recv(1024)
@@ -19,9 +13,34 @@ try:
             break
         print('Número recebido: %s' %data.decode())
         i+=1
-        if i>=2:
-            break
     connection.send(str(soma).encode())
+
+class sumThread(threading.Thread):
+    def __init__(self, connection):
+        threading.Thread.__init__(self)
+        self.connection = connection
+
+    def run(self):
+        print('Conexão estabelecida.\n')
+        sum(self.connection)
+        print('Fechando conexão.\n')
+        self.connection.close()
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+server.bind (('localhost', 7777))
+print('Aguardando conexões.\n')
+server.listen(1)
+
+i=0
+soma=0
+try:
+    print("Aguardando mensagem do cliente.\n")
+    while True:
+        connection, address = server.accept()
+        newThread = sumThread(connection)
+        newThread.start()
+        
 except socket.error as e: 
     print ("Socket error: %s" %str(e)) 
 except Exception as e: 
